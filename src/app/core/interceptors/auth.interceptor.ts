f338788eee62627b8a38082a41201f2f30e82695
Chris Turner <chris@forloop.space>
2020-07-04 19:16:26 +0100

    fix: Clean auth interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,29 +1,24 @@
-import {Injectable} from '@angular/core';
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
-import {from, Observable} from 'rxjs';
-import {AngularFireAuth} from '@angular/fire/auth';
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private angularFireAuth: AngularFireAuth) {
-  }
+  constructor(private angularFireAuth: AngularFireAuth) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return from(this.handleAccess(request, next));
-  }
-
-  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
-    await this.angularFireAuth.user.subscribe(user => {
-      user.getIdToken().then(
-        token =>
-          (request = request.clone({
+    return this.angularFireAuth.idToken.pipe(
+      mergeMap((token) => {
+        return next.handle(
+          request.clone({
             setHeaders: {
-              Authorization: 'Bearer ' + token
-            }
-          }))
-      );
-    });
-
-    return next.handle(request).toPromise();
+              Authorization: 'Bearer ' + token,
+            },
+          })
+        );
+      })
+    );
   }
 }
